78a065fc011f907880cb43cdbaaf9f53c1130635
shindannin <shindannin@gmail.com>
2019-04-10 08:53:18 +0800

    パッケージのアップデートとホームページの色の差し替え 色の変更

--- joseki_viewer/color.h
+++ joseki_viewer/color.h
@@ -3,5 +3,5 @@
 #include <Siv3D.hpp>
 constexpr s3d::Color COLOR_LINK_BEST(64, 255, 64, 128);
 constexpr s3d::Color COLOR_LINK_NORMAL(192, 192, 192, 128);
-constexpr s3d::Color COLOR_SENTE_DARK(64, 0, 0, 128);
-constexpr s3d::Color COLOR_GOTE_DARK(0, 0, 64, 128);
+constexpr s3d::Color COLOR_SENTE_DARK(48, 0, 0, 192);
+constexpr s3d::Color COLOR_GOTE_DARK(0, 0, 48, 192);
--- package.bat
+++ package.bat
@@ -3,22 +3,21 @@ MKDIR application
 MKDIR application\pictures
 MKDIR application\sounds
 MKDIR application\Engine\dll(x86)
-XCOPY Release\joseki_viewer.exe                    application
+XCOPY Release\siv3d_shogi_viewer.exe               application
 XCOPY /S /Y Siv3D(August2016v2)1\pictures          application\pictures
 XCOPY /S /Y Siv3D(August2016v2)1\sounds            application\sounds
-XCOPY /S /Y Siv3D(August2016v2)1\Engine\Config.ini application\Engine
 XCOPY /S /Y Siv3D(August2016v2)1\Engine\dll(x86)   application\Engine\dll(x86)
-
-"C:\Program Files\7-Zip\7z.exe" a joseki_viewer.zip  application
+XCOPY /S /Y Siv3D(August2016v2)1\Engine\Config.ini application\Engine
+"C:\Program Files\7-Zip\7z.exe" a siv3d_shogi_viewer.zip  application
 
 DEL /Q application_x64
 MKDIR application_x64
 MKDIR application_x64\pictures
 MKDIR application_x64\sounds
 MKDIR application_x64\Engine\dll(x64)
-XCOPY x64\Release\joseki_viewer.exe                application_x64
+XCOPY x64\Release\siv3d_shogi_viewer.exe           application_x64
 XCOPY /S /Y Siv3D(August2016v2)1\pictures          application_x64\pictures
 XCOPY /S /Y Siv3D(August2016v2)1\sounds            application_x64\sounds
-XCOPY /S /Y Siv3D(August2016v2)1\Engine\Config.ini application\Engine
 XCOPY /S /Y Siv3D(August2016v2)1\Engine\dll(x64)   application_x64\Engine\dll(x64)
-"C:\Program Files\7-Zip\7z.exe" a joseki_viewer_x64.zip  application_x64
+XCOPY /S /Y Siv3D(August2016v2)1\Engine\Config.ini application_x64\Engine
+"C:\Program Files\7-Zip\7z.exe" a siv3d_shogi_viewer_x64.zip  application_x64
